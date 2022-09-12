a5602c746515b8232fb79388dd6a192f1db33d29
Shtrohaim <79344587+Shtrohaim@users.noreply.github.com>
2022-09-12 18:32:31 +0700

    install webpack and add header

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+module.exports = {
+    mode: 'development',
+   entry: './src/index.js',
+   output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+   },
+   module: {
+      rules: [{
+         test:/\.(s*)css$/,
+         use: [
+            miniCss.loader,
+            'css-loader',
+            'sass-loader',
+         ]
+      }]
+   },
+   plugins: [
+      new miniCss({
+         filename: 'style.min.css',
+      }),
+   ],
+   optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+   devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+  },
+   
+};
